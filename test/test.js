006d9b8ce215091695c79a2ae34c7fee55d5ce9f
Kanishka-Bhambhani <49683557+Kanishka-Bhambhani@users.noreply.github.com>
2020-07-05 22:23:10 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -2,9 +2,18 @@ var assert = require('assert');
 var co2Calculator = require('../co2-calculator.js');
 
 describe('suite-1', () => {
-    it('calculateCo2Emission test', () => {
+    it('calculateCo2Emission test 1', () => {
       assert.equal( co2Calculator.calculateCo2Emission('large-petrol-car',1800.5,'km') , 507741 );
     });
+    it('calculateCo2Emission test 3', () => {
+      assert.equal( co2Calculator.calculateCo2Emission('large-petrol-car',1800.5,'m') , 507.741 );
+    });
+    it('calculateCo2Emission test 4', () => {
+      assert.equal( co2Calculator.calculateCo2Emission('large-petrol-car',1800.5,) ,  507741 );
+    });
+    it('calculateCo2Emission test 5', () => {
+      assert.equal( co2Calculator.calculateCo2Emission('large-petrol-car',) ,  undefined );
+    });	
   });
 
 describe('suite-2', () => {
@@ -49,3 +58,17 @@ describe('suite-4', () => {
     });
   });
 
+describe('suite-5', () => {
+    it('Print co2 emissions based on unitOfEmission test 1', () => {
+      assert.equal( co2Calculator.printCo2Emission(507741,'kg') , '507.741kg' );
+    });
+    it('Print co2 emissions based on unitOfEmission test 2', () => {
+      assert.equal( co2Calculator.printCo2Emission(507741,'g') , '507741g' );
+    });
+    it('Print co2 emissions based on unitOfEmission test 3', () => {
+      assert.equal( co2Calculator.printCo2Emission(507741,) ,  '507.741kg' );
+    });
+    it('Print co2 emissions based on unitOfEmission test 4', () => {
+      assert.equal( co2Calculator.printCo2Emission(undefined,'kg') ,  'Incorrect Data' );
+    });	
+  });
